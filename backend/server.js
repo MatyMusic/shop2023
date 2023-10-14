fcc69d693cac53515ff3133bb2b03b2867fab5e7
maty <mtyg7702@gmail.com>
2023-10-14 22:20:57 +0300

    and 2

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,85 @@
+// import path from "path";
+// import express from "express";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+// import connectDB from "./config/db.js";
+// import productRoutes from "./routes/productRoutes.js";
+// import userRoutes from "./routes/userRoutes.js";
+// import orderRoutes from "./routes/orderRoutes.js";
+// import uploadRoutes from "./routes/uploadRoutes.js";
+// import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+// import cookieParser from "cookie-parser";
+
+// const port = process.env.PORT || 8000;
+// connectDB();
+
+// const app = express();
+
+// //? body parser middleware
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// //? cookie parser middleware
+// app.use(cookieParser());
+
+// app.get("/", (req, res) => {
+//   res.send("api is running");
+// });
+
+// app.use("/api/products", productRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/orders", orderRoutes);
+// app.use("/api/upload", uploadRoutes);
+
+// app.get("/api/config/paypal", (req, res) =>
+//   res.send({
+//     clientId: process.env.PAYPAL_CLIENT_ID,
+//   })
+// );
+
+// const __dirname = path.resolve();
+// app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// // if (process.env.NODE_ENV === "production") {
+// //   const __dirname = path.resolve();
+// //   app.use("/uploads", express.static("/var/data/uploads"));
+// //   app.use(express.static(path.join(__dirname, "/fronend/build")));
+
+// //   app.get("*", (req, res) =>
+// //     res.sendFile(path.resolve(__dirname, "fronend", "build", "index.html"))
+// //   );
+// // } else {
+// //   const __dirname = path.resolve();
+// //   app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+// //   app.get("/", (req, res) => {
+// //     res.send("API is running....");
+// //   });
+// // }
+
+// if (process.env.NODE_ENV === "production") {
+//   //set static folder
+//   app.use(express.static(path.join(__dirname, "/fronend/build")));
+
+//   //any route that is not api will be redirected to index.html
+//   app.get("*", (req, res) =>
+//     res.sendFile(__dirname, "fronend", "build", "index.html")
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("api is running");
+//   });
+// }
+
+// app.use(notFound);
+// app.use(errorHandler);
+
+// app.listen(port, () => console.log(`server running on port ${port}`));
+
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
@@ -9,23 +87,16 @@ import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import cookieParser from "cookie-parser";
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
+
 connectDB();
 
 const app = express();
 
-//? body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-//? cookie parser middleware
 app.use(cookieParser());
-
-app.get("/", (req, res) => {
-  res.send("api is running");
-});
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -33,45 +104,28 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 
 app.get("/api/config/paypal", (req, res) =>
-  res.send({
-    clientId: process.env.PAYPAL_CLIENT_ID,
-  })
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
-// if (process.env.NODE_ENV === "production") {
-//   const __dirname = path.resolve();
-//   app.use("/uploads", express.static("/var/data/uploads"));
-//   app.use(express.static(path.join(__dirname, "/fronend/build")));
-
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname, "fronend", "build", "index.html"))
-//   );
-// } else {
-//   const __dirname = path.resolve();
-//   app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-//   app.get("/", (req, res) => {
-//     res.send("API is running....");
-//   });
-// }
-
 if (process.env.NODE_ENV === "production") {
-  //set static folder
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static("/var/data/uploads"));
   app.use(express.static(path.join(__dirname, "/fronend/build")));
 
-  //any route that is not api will be redirected to index.html
   app.get("*", (req, res) =>
-    res.sendFile(__dirname, "fronend", "build", "index.html")
+    res.sendFile(path.resolve(__dirname, "fronend", "build", "index.html"))
   );
 } else {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
   app.get("/", (req, res) => {
-    res.send("api is running");
+    res.send("API is running....");
   });
 }
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`server running on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
